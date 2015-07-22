847d29df787d3f76fb6f5a1a6e8ee5c16e22f76a
bhattars <sagar.bhattarai@merck.com>
2015-07-22 17:34:27 -0400

    -- R_100_ch2
    completed exercises 7 and 8.

---
+++ examples/ex7.rb
@@ -0,0 +1,23 @@
+#Lrnrby - Example in exercise 7, More Printing
+
+puts "Mary had a little lamb."
+#snow is not a variable it is a string (single quote leaves string as strign"), double quote treats  character inside #{} as variable
+puts "Its fleece was white as #{'snow'}."
+puts "And everywhere that Mary went."
+#multiplies "." to times
+puts "." * 10 # what'd that do?
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 ="r"
+#watch that print vs. puts on this line what's it do ?
+print end1 + end2 + end3 + end4 + end5 + end6 
+puts end7 + end8 + end9 + end10 + end11 + end12
---
+++ examples/ex8.rb
@@ -0,0 +1,16 @@
+#Exercise 8 - Printing, printing ---  creating format string, rather than using variables.
+formatter = "%{first} %{second} %{third}  #%{fourth}"
+
+puts formatter % {first: 1, second: 2, third: 3, fourth: 4}
+puts formatter % {first: "one", second: "two", third: "three", fourth: "four"}
+puts formatter % {first: true, second: false, third: true, fourth: false}
+puts formatter % {first: formatter, second: formatter, third: formatter, fourth: formatter}
+
+puts formatter % {
+	first: "I had this thing.",
+	second: "That you could type up right.",
+	third: "but it didn't sing.",
+	fourth: "So I said goodnight."
+	}
+
+# come back, review it later. 
